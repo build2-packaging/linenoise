c62f9e435b521cac5aa85f723114c527ca1c9646
lyrahgames <lyrahgames@mailbox.org>
2023-10-18 15:43:07 +0200

    Improve some test
    - Call non-inline functions to test linking
    - Use `testscript` for basic functionality
    
    Issue: #1

--- liblinenoise/tests/basics/driver.c
+++ liblinenoise/tests/basics/driver.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//
 #include <linenoise.h>
 
-int main() {}
+int main() {
+  while (1) {
+    char* line = linenoise("prompt> ");
+    if (!line) break;
+
+    printf("echo: '%s'\n", line);
+    linenoiseHistoryAdd(line);
+    linenoiseHistorySave("history.txt");
+
+    free(line);
+  }
+
+  return 0;
+}
